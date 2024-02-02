b5dc6568c9b1b0ecc2b7321b777af81c1391bc32
Olga <olga-ork@yandex.ru>
2024-02-02 17:41:20 +0300

    Client server

---
+++ chat-server/src/main/java/olga/junior/chat/server/ClientManager.java
@@ -0,0 +1,95 @@
+package olga.junior.chat.server;
+
+import java.io.*;
+import java.net.Socket;
+import java.util.ArrayList;
+
+public class ClientManager implements Runnable{
+    private Socket socket;
+    private BufferedReader bufferedReader;
+    private BufferedWriter bufferedWriter;
+    private  String name;
+
+    public static ArrayList<ClientManager> clients = new ArrayList<>();
+
+    public ClientManager(Socket socket) {
+        try {
+            this.socket = socket;
+            bufferedWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+            bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            name = bufferedReader.readLine();
+            clients.add(this);
+            System.out.println(name + " подключился к чату.");
+            broadcastMessage("Server: " + name + " подключился к чату.");
+        } catch (IOException e) {
+            closeEverything(socket, bufferedReader, bufferedWriter);
+        }
+    }
+
+    @Override
+    public void run() {
+        String massageFromClient;
+        while (socket.isConnected()){
+            try {
+                // Чтение данных
+                massageFromClient = bufferedReader.readLine();
+                if (massageFromClient == null) {
+                    // для macOS
+                    closeEverything(socket, bufferedReader, bufferedWriter);
+                    break;
+                }
+                // Отправка данных всем слушателям
+                broadcastMessage(massageFromClient);
+            } catch (IOException e) {
+                closeEverything(socket, bufferedReader, bufferedWriter);
+                break;
+            }
+        }
+    }
+
+    /**
+     * Отправка сообщения всем слушателям
+     * сообщение
+     */
+
+    public void broadcastMessage(String message) {
+        for (ClientManager client : clients) {
+            try {
+                if (!client.name.equals(name) && message != null) {
+                    client.bufferedWriter.write(message);
+                    client.bufferedWriter.newLine();
+                    client.bufferedWriter.flush();
+                }
+            } catch (IOException e) {
+                closeEverything(socket, bufferedReader, bufferedWriter);
+            }
+        }
+    }
+
+    private void closeEverything(Socket socket, BufferedReader bufferedReader, BufferedWriter bufferedWriter) {
+        // Удаление клиента из коллекции
+        removeClient();
+        try {
+            if (bufferedReader != null) {
+                bufferedReader.close();
+            }
+            if (bufferedWriter != null) {
+                bufferedWriter.close();
+            }
+            if (socket != null) {
+                socket.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Удаление клиента из коллекции
+     */
+    public void removeClient() {
+        clients.remove(this);
+        System.out.println(name + " покинул чат.");
+        broadcastMessage("Server: " + name + " покинул чат.");
+    }
+}
---
+++ chat-server/src/main/java/olga/junior/chat/server/Program.java
@@ -0,0 +1,18 @@
+package olga.junior.chat.server;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+
+public class Program {
+    public static void main(String[] args) {
+        try {
+            ServerSocket serverSocket = new ServerSocket(5000);
+            Server server = new Server(serverSocket);
+            server.runServer();
+
+        }
+        catch (IOException e){
+            throw new RuntimeException(e);
+        }
+    }
+}
